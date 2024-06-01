e1e94432ce9087714193f0c653303f8e99340b68
parth <kparth559@gmail.com>
2024-06-01 18:15:39 +0530

    project1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  //
+  // let str = "Hello"
+  // let str2 = "World"
+
+  // if(str === "Hello" && str2 === "World")
+  //   {
+  //     console.log("Hello World")
+  //   }
+   const student = {
+    student_name: "Aman", 
+    
+   }
+   const student2 = {
+    student_name: "Aman",
+   }
+
+   const fun = () =>{
+    let a = 1;
+    let b = 2;
+    let c = a+b;
+    console.log(c)
+   }
+  if(student.student_name === student2.student_name)
+{
+  fun()
+}  
+else{
+  console.log("Hello world")
+}
+
   return (
     <div className="App">
       <header className="App-header">
