60b9a330a68690c252b4ab98707d86fe95d98335
parth <kparth559@gmail.com>
2024-06-20 18:21:54 +0530

    store-array in local storage

--- src/pages/Students.tsx
+++ src/pages/Students.tsx
@@ -1,13 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { IStudent } from '../interface/student';
+import "./Student.scss"
 
 const Students : React.FC= () => {
+  const [allStudents, setAllStudents]= useState<IStudent[]>();
+
+
+  useEffect(() => {
+    const student = JSON.parse(localStorage.getItem("students") as string);
+    
+    setAllStudents(student)
+    console.log(" student data", student)
+
+  },[]);
+
   return (
     <>
-    <button title="CreateStudent">
+    <button title="CreateStudent" className="create_button">
         <Link to="/student/create-student"> Create-Student Details </Link>
     </button>
+    
+    <table >
+        <thead>
+          <tr > 
+            <td>Sl.</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Phone Number</td>
+            <td>Email ID</td>
+          </tr>
+        </thead>
+        <tbody>
+          {allStudents?.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.phone}</td>
+                <td>{item.email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    
     </>
+
+
   )
 }
 
